329f97f10640199b616291c6db10f2373df5b87d
Jeroen Bloemscheer <jeroen.bloemscheer@merkator.com>
2026-04-30 09:24:03 +0200

    refactor: remove commented-out code from ExampleStepAttributes constructor for cleaner example

--- examples/Allure.Xunit.Examples/ExampleStepAttributes.cs
+++ examples/Allure.Xunit.Examples/ExampleStepAttributes.cs
@@ -11,8 +11,6 @@ public class ExampleStepAttributes : IDisposable
     [AllureBefore("Initialization in constructor")]
     public ExampleStepAttributes()
     {
-        // AddAttachment();
-        // NestedStep(1);
         NestedStepReturningString("Second");
     }
 
